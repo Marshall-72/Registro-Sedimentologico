853356e4b2afe96244abe7976491c1ef0f717449
Marshall-72 <claudiomarcial061@gmail.com>
2025-06-04 01:29:39 -0500

    app.py

--- app.py
+++ app.py
@@ -57,12 +57,11 @@ if uploaded_file is not None:
         # Agregar la descripción a la derecha de la barra
         ax.text(fixed_length + 0.05, y_pos[i] - 0.2, row['Descripcion'], va='center', fontsize=8, color='black')
 
-    # Colocar las etiquetas de los contactos entre las barras (profundidades)
-    depth = 0  # Comenzamos en 0 m para el primer estrato
+    # Colocar las etiquetas de los contactos entre las barras (usando las profundidades de inicio y fin)
     for i, row in data.iterrows():
-        # Agregar la etiqueta de profundidad al final de cada estrato
-        depth += row['Espesor (m)']  # Calculamos la profundidad final
-        ax.text(fixed_length + 0.05, y_pos[i], f"{depth} m", va='center', fontsize=10, color='black')
+        # Colocamos la etiqueta en el contacto entre las barras, utilizando la "Profundidad Fin (m)"
+        depth_end = row['Profundidad Fin (m)']
+        ax.text(fixed_length + 0.05, y_pos[i], f"{depth_end} m", va='center', fontsize=10, color='black')
 
     # Eliminar el nombre del eje Y
     ax.set_ylabel('')  
