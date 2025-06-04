a42ba08ac1622a11e292cb526ff28baafa3e86da
Marshall-72 <claudiomarcial061@gmail.com>
2025-06-04 01:09:12 -0500

    app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ if uploaded_file is not None:
     fig, ax = plt.subplots(figsize=(10, 10))
 
     # Longitud fija para todas las barras (independientemente del espesor)
-    fixed_length = 3  # Por ejemplo, 10 metros de longitud fija
+    fixed_length = 0.00000000000000000001  # Por ejemplo, 10 metros de longitud fija
 
     # Inicializar la posición de la barra (y-axis)
     y_pos = np.arange(len(data))
